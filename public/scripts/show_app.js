5197526570538d5a7bb983f8dcb315ae8fa8ae40
Scott Veale <srveale42@gmail.com>
2017-02-02 02:28:59 +0000

    added partials

---
+++ public/scripts/show_app.js
@@ -0,0 +1,10 @@
+$(() => {
+  $.ajax({
+    method: "GET",
+    url: "/"
+  }).done((resources) => {
+    for(resource of resources) {
+      $("<div>").html(pin.title).appendTo($("body"));
+    }
+  })
+});
