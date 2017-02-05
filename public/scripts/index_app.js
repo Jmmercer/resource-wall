40f023e59d3f0941b477e66e0c5f3ce73918722e
Hafiz Suara <hafbau@yahoo.com>
2017-02-05 03:38:02 +0000

    added category seed

--- public/scripts/index_app.js
+++ public/scripts/index_app.js
@@ -7,7 +7,7 @@ const createResource = function(resource) {
       <em><a class="res-url" href="${resource.url}" target="_blank">source</a></em>
       <a class="" href="#0"><span class="r-action likes glyphicon glyphicon-heart-empty">${resource.likes_count}</span></a>
       <span><em>comments: ${resource.comments_count}</em><span>
-      <span>rating: <a class="r-action ratings" href='#0' >${resource.avg_rating}</a><span>
+      <span>rating: <a class="" href='#0' >${resource.avg_rating}</a><span>
     </figure>`)
 }
 
@@ -29,43 +29,60 @@ const newCommentForm = $(`<form method="POST" action="http://localhost:8080/twee
           <button>Go</button>
         </form>`);
 
+// const loggedInState = function(isLoggedIn) { return $(`<input type="checkbox" checked=${isLoggedIn}`) }
+
 $(() => {
 
   // Handling Likes TODO: handle rating
-  $("#maincontent").on("click", "a.r-action", function(event) {
+  $("#maincontent").on("click", ".r-action", function(event) {
     const $this = $(event.target);
-    const action = ($this.attr('class')).includes('likes') ? 'likes' : 'ratings';
+    const action = $this.hasClass('likes') ? 'likes' : 'ratings';
     const res_id = $this.closest('.h-resource').data('res_id');
+    const inputId = $this.attr('for');
+    const data = (action === 'ratings') ? (6 - Number(inputId[2])) : undefined;
 
     $.ajax({
       url: `/resources/${res_id}/${action}`,
+      data: {value: data}
     }).done(function(newValue) {
-      $this.text(newValue);
+      if (data) {
+        const $wrapper = $this.closest('.wrapper')
+        $wrapper.next('.avg-rating').text(newValue);
+        $wrapper.find('input').prop('checked', false);
+        $(inputId).prop('checked', true);
+      } else {
+        $this.text(newValue);
+      }
     });
     return false;
   });
 
+  // Stop propagation 
+
   // Show a resource
-  $("#maincontent").on("click", function(event) {
+  $("#maincontent").on("click", ".h-resource", function(event) {
     const $target = $(event.target);
-    const targetClass = $target.attr("class");
     const $this = $(this);
     const $thisResource = $target.closest('.h-resource');
-    if (targetClass && targetClass.includes("res-url")) {
+    if ($target.hasClass("res-url") || $target.is('input') || $target.is('label')) {
     } else {
-      $this.children().remove();
+      $("#maincontent").children().remove();
       $thisResource.css("min-width","650px");
-      $this.append($thisResource);
-      $this.append(newCommentForm);
+      $("#maincontent").append($thisResource)
+
+      $.ajax({
+        url: `/resources/${$thisResource.data('res_id')}/comments`,
+      }).done(function(result) {
+        if(result.isLoggedIn) {
+          $thisResource.find('.wrapper').show();
+          $thisResource.append(newCommentForm);
+        }
+        result.comments.forEach(function(comment) {
+          $thisResource.append(createComment(comment));
+        });
+      });
     }
-    $.ajax({
-      url: `/resources/${$thisResource.data('res_id')}/comments`,
-    }).done(function(comments) {
-      comments.forEach(function(comment) {
-        $this.append(createComment(comment));
-      })
-    })
-  })
+  });
 
   //Handling Search
   $("#search-form").on("submit", function(event) {
